2f631784dbfa205990d93f0b14e76bad670f1d3a
arthurlorenzett <arthurhenriquelorenzett@gmail.com>
2026-06-23 18:43:50 -0300

    Corrigindo api

--- app/models/api_client.py
+++ app/models/api_client.py
@@ -1,13 +1,13 @@
-"""import requests
+import requests
 
 class ApiClient:
     def __init__(self):
-        self.base_url = "http://localhost:5000"
+        self.base_url = "https://sistema-locacao-715o.onrender.com"
 
     def cadastrar_usuario(self, payload):
         try:
             r = requests.post(f"{self.base_url}/usuarios", json=payload, timeout=5)
             return r.json(), r.status_code
         except requests.exceptions.ConnectionError:
-            return {"erro": "Sem conexão com o servidor."}, 0 """
+            return {"erro": "Sem conexão com o servidor."}, 0 
 #teste para ver se o arquivo é necessário, caso não seja, pode ser deletado.
